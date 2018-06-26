4746120787cfa6df143b424aec9ac776d753bf33
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-26 19:20:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# dog.rb
+class Dog 
+  
+  def initialize(fido, snoopy, lassie)
+    @fido = fido
+    @snoopy = snoopy
+    @lassie = lassie 
+  end
+  
+  def fido
+    @fido
+  end
+  
+  def snoopy
+    @snoopy
+  end
+  
+  def lassie
+    @lassie
+  end
